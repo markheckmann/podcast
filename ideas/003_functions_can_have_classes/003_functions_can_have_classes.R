efb55ce1693b153df6f4fd21fe29c58e5b0866aa
Mark Heckmann <mark.heckmann@deutschebahn.com>
2025-01-08 11:26:04 +0100

    more notes

--- ideas/003_functions_can_have_classes/003_functions_can_have_classes.R
+++ ideas/003_functions_can_have_classes/003_functions_can_have_classes.R
@@ -1,18 +1,19 @@
 library(cli)
 
-paste()
-paste
+cat("hello")
+cat
 
 col_grey("hello")
 col_grey
 
-is.function(paste)
+is.function(cat)
 is.function(col_grey)
 
-class(paste)
+class(cat)
 class(col_grey)
 
-cli:::print.cli_ansi_style
+cli:::print.cli_ansi_style # x is the fucntion
+# NOTE: col_grey = print(col_grey)
 
 df <- sloop::s3_methods_generic("print")
 stringr::str_subset(df$class, "cli")
@@ -27,3 +28,11 @@ print.foo <- \(x, ...) {
 }
 foo(1)
 foo
+
+# how to print it then?
+print.default(col_grey)
+print.function(col_grey)
+
+# when is this useful?
+
+
