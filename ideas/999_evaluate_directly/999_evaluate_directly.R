c43c1f4ef958d97100382d910ffd92fe3cbc5e01
Mark Heckmann <mark.heckmann@deutschebahn.com>
2025-06-06 14:29:24 +0200

    add idea

---
+++ ideas/999_evaluate_directly/999_evaluate_directly.R
@@ -0,0 +1,21 @@
+# use case of parenthesis indirectly in code (r <- r+ 1)
+
+wb <- createWorkbook()
+
+sheet <- "Ausschluss"
+addWorksheet(wb, sheet)
+writeData(wb, sheet, df_ausschluss)
+
+sheet <- "Bereich"
+addWorksheet(wb, sheet)
+writeData(wb, sheet, df_bereich_ma)
+
+sheet <- "Demo"
+addWorksheet(wb, sheet)
+r <- 1
+writeData(wb, sheet, "xxx", startRow = (r <- r + 2))
+writeData(wb, sheet, df_fuehrung, startRow = (r <- r + 2))
+writeData(wb, sheet, "yyy", startRow = (r <- r + nrow(df_fuehrung) + 3))
+writeData(wb, sheet, df_standort, startRow = (r <- r + 2))
+writeData(wb, sheet, "zzz", startRow = (r <- r + nrow(df_standort) + 3))
+writeData(wb, sheet, df_prod_verw, startRow = (r <- r + 2))
